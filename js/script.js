59f78eeb0acf6d806ea3f251228ef7588dd17a38
Lilber <emaksishkin@mail.ru>
2022-03-13 19:43:58 +0300

    thirdDay

--- js/script.js
+++ js/script.js
@@ -15,4 +15,44 @@ if (productsCard.length) {
   });
 }
 
+let button = document.getElementById("header__link-button-company");
+let navigation1 = document.getElementById("navigation-menu");
 
+button.addEventListener("click", function () {
+  navigation1.classList.toggle("header__item_start-animation");
+});
+
+let buttonCompany = document.getElementById("header__link-button-company");
+let navigation = document.getElementById("navigation-menu");
+let littleLoophole = document.querySelectorAll(".little-loophole");
+//let place = document.getElementById("header-place");
+buttonCompany.addEventListener("mouseenter", () => {
+  navigation.classList.remove("header__item_end-animation");
+  navigation.classList.add("header__item_start-animation");
+});
+//place.addEventListener("mouseenter", () => {
+//  navigation.classList.remove("header__item_end-animation");
+//  navigation.classList.add("header__item_start-animation");
+//});
+navigation.addEventListener("mouseleave", () => {
+  navigation.classList.remove("header__item_start-animation");
+  navigation.classList.add("header__item_end-animation");
+});
+
+function onEntry(entry) {
+  entry.forEach((change) => {
+    if (change.isIntersecting) {
+      change.target.classList.add("element-show");
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5],
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll(".element-animation");
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
